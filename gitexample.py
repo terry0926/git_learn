71e87ff4166fdb5194dffb209655fea723901f79
terry0926 <huangterry0926@gmail.com>
2026-02-04 17:14:28 +0800

    t

--- gitexample.py
+++ gitexample.py
@@ -1,7 +1,7 @@
 import requests
 TOKEN = "8004843742:AAHm-3TXXS5hkbdtWt6byclIDz58XpvXvHk"
 CHAT_ID = "7982846707"
-MESSAGE = "047"
+MESSAGE = "047grace"
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 payload = {"chat_id": CHAT_ID, "text": MESSAGE}
 response = requests.post(url, json=payload)
